9e4db6e0640d86a1b57b5d2f1f484fa872b7e84c
Graham Preston <graham.preston@gmail.com>
2017-06-30 22:08:50 -0400

    Update registration

--- pages/forms.py
+++ pages/forms.py
@@ -1,20 +1,22 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.utils.safestring import mark_safe
+
+red_asterisk = mark_safe(' <font color="red">*</font>')
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(max_length=20, help_text='20 characters or fewer. Letters, digits and @/./+/-/_ only.')
+    email_address = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = (
             'username',
-            'first_name',
-            'last_name',
             'password1',
             'password2',
-            'email',
+            'email_address',
         )
 
     def save(self, commit=True):
--- pages/models.py
+++ pages/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, models.CASCADE)
     website = models.URLField(default='')
     description = models.CharField(default='', max_length=200)
+
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile, sender=User)
