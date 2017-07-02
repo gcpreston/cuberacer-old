9e20460b78dca86a5270efe741b8f7deccb01980
Graham Preston <graham.preston@gmail.com>
2017-07-01 21:19:12 -0400

    Implement basic timer

--- timer/static/timer/timer.js
+++ timer/static/timer/timer.js
@@ -1,13 +1,34 @@
-var initial = new Date();
+var initial;
+var timer;
+var running = false;
+var buffer = false;
 
 function startTime() {
-    var timer = setInterval(updateTime, 1);
+    document.getElementById("time").innerHTML = 0;
+    running = true;
+    initial = new Date();
+    timer = setInterval(updateTime, 1);
 }
 
 function updateTime() {
-    document.getElementById("time").innerHTML = new Date() - initial;
+    document.getElementById("time").innerHTML = (new Date() - initial) / 1000;
 }
 
 function stopTime() {
     window.clearInterval(timer);
+    running = false;
 }
+
+$(document).on('keyup', function(e) {
+    if (e.which == 32 && !running && !buffer)
+        startTime();
+    else
+        buffer = false;
+});
+
+$(document).on('keydown', function(e) {
+    if (e.which == 32 && running) {
+        stopTime();
+        buffer = true;
+    }
+});
